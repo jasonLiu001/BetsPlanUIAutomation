1b883f38288fd266632da31c1c9629bf32f24e09
jasonLiu001 <wangliu@isoftstone.com>
2019-08-25 21:02:10 +0800

    方法提取

--- BetsPlanUIAutomation/Main.cs
+++ BetsPlanUIAutomation/Main.cs
@@ -15,48 +15,8 @@ namespace BetsPlanUIAutomation
         private void Btn_Start_Click(object sender, EventArgs e)
         {
             this.UpdateMessage("监控已启动...");
-            //查找挂机软件窗口对象
-            var rootElement = ControlViewFinder.GetAutomationElementByProcessName("星图官方挂机软件");
-            if (rootElement == null)
-            {
-                this.UpdateMessage("未查找到星图官方挂机软件窗口");
-                return;
-            }
 
-            //包含 自动投注选项卡Tab 的父窗口
-            var autoBetsFirstElement = ControlViewFinder.GetFirstChild(rootElement, 2);
-            if (autoBetsFirstElement == null)
-            {
-                this.UpdateMessage("未查找到包含自动投注Tab选项卡所在的父窗口");
-                return;
-            }
-
-            //包含 所有选项卡 的父窗口
-            var autoBetsElement = TreeWalker.ControlViewWalker.GetLastChild(autoBetsFirstElement);
-            if (autoBetsElement == null)
-            {
-                this.UpdateMessage("未查找到包含所有Tab选项卡的父窗口");
-                return;
-            }
-
-            //包含 表格 最大盈利 值的父窗口
-            var winAutoSiblingElement = ControlViewFinder.GetFirstChild(autoBetsElement, 6);
-            if (winAutoSiblingElement == null)
-            {
-                this.UpdateMessage("未查找到包含表格及最大盈利数据的父窗口");
-                return;
-            }
-
-            //包含 模拟盈亏记录 父窗口
-            var winAutoElement = TreeWalker.ControlViewWalker.GetLastChild(winAutoSiblingElement);
-            if (winAutoElement == null)
-            {
-                this.UpdateMessage("未查找到包含最大盈利数据的父窗口");
-                return;
-            }
-
-            //模拟盈亏记录窗口中 第一个元素 兄弟控件
-            var winAutoFirstElement = TreeWalker.ControlViewWalker.GetFirstChild(winAutoElement);
+            var winAutoFirstElement = this.GetWinAutoFirstElement();
             if (winAutoFirstElement == null)
             {
                 this.UpdateMessage("未查找到真实盈亏字段的兄弟控件");
@@ -72,6 +32,57 @@ namespace BetsPlanUIAutomation
             }
 
             this.UpdateMessage($"当前真实盈亏：{winAutoControl.Current.Name}");
+        }
+
+        /// <summary>
+        /// 查找模拟盈亏字段所在的同级字段
+        /// </summary>
+        /// <returns>null 或者 AutomationElement</returns>
+        private AutomationElement GetWinAutoFirstElement()
+        {
+            //查找挂机软件窗口对象
+            var rootElement = ControlViewFinder.GetAutomationElementByProcessName("星图官方挂机软件");
+            if (rootElement == null)
+            {
+                this.UpdateMessage("未查找到星图官方挂机软件窗口");
+                return null;
+            }
+
+            //包含 自动投注选项卡Tab 的父窗口
+            var autoBetsFirstElement = ControlViewFinder.GetFirstChild(rootElement, 2);
+            if (autoBetsFirstElement == null)
+            {
+                this.UpdateMessage("未查找到包含自动投注Tab选项卡所在的父窗口");
+                return null;
+            }
+
+            //包含 所有选项卡 的父窗口
+            var autoBetsElement = TreeWalker.ControlViewWalker.GetLastChild(autoBetsFirstElement);
+            if (autoBetsElement == null)
+            {
+                this.UpdateMessage("未查找到包含所有Tab选项卡的父窗口");
+                return null;
+            }
+
+            //包含 表格 最大盈利 值的父窗口
+            var winAutoSiblingElement = ControlViewFinder.GetFirstChild(autoBetsElement, 6);
+            if (winAutoSiblingElement == null)
+            {
+                this.UpdateMessage("未查找到包含表格及最大盈利数据的父窗口");
+                return null;
+            }
+
+            //包含 模拟盈亏记录 父窗口
+            var winAutoElement = TreeWalker.ControlViewWalker.GetLastChild(winAutoSiblingElement);
+            if (winAutoElement == null)
+            {
+                this.UpdateMessage("未查找到包含最大盈利数据的父窗口");
+                return null;
+            }
+
+            //模拟盈亏记录窗口中 第一个元素 兄弟控件
+            var winAutoFirstElement = TreeWalker.ControlViewWalker.GetFirstChild(winAutoElement);
+            return winAutoFirstElement;
         }
 
         /// <summary>
